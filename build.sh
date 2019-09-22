ae2aafb783cdcbaf8159b7d7a44297a973b60b99
Rajat Jindal <rajatjindal83@gmail.com>
2019-09-22 22:15:33 +0530

    more hacking into build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,11 @@ rm -rf _dist || true
 mkdir -p _dist
 # env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
 env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/darwin-amd64/kubectl-modify-secret      main.go
-tar -cvzf _dist/darwin-amd64-$version.tar.gz _dist/darwin-amd64
+cd _dist/darwin-amd64/ && tar -cvzf darwin-amd64-$version.tar.gz kubectl-modify-secret
+
+cd -
 
 env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/linux-amd64/kubectl-modify-secret       main.go
-tar -cvzf _dist/linux-amd64-$version.tar.gz _dist/linux-amd64
+cd _dist/linux-amd64/  && tar -cvzf linux-amd64-$version.tar.gz kubectl-modify-secret
+
+cd -
