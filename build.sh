ef941eb51fea6ccceb546501c3d5b78ad6c52b49
Rajat Jindal <rajatjindal83@gmail.com>
2019-09-22 22:05:32 +0530

    create tar.gz file for krew release

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ if [ -z "$version" ]; then
     exit -1
 fi
 
+rm -rf _dist || true
 mkdir -p _dist
 # env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
-env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-darwin-amd64-$version      main.go
-env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-linux-amd64-$version       main.go
+env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/darwin-amd64/kubectl-modify-secret      main.go
+tar -cvzf _dist/darwin-amd64-$version.tar.gz _dist/darwin-amd64
+
+env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/linux-amd64/kubectl-modify-secret       main.go
+tar -cvzf _dist/linux-amd64-$version.tar.gz _dist/linux-amd64
