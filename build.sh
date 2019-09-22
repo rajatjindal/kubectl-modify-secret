3376ab57b7f8565892695a90a2d6539cf64e29b8
Rajat Jindal <rajatjindal83@gmail.com>
2019-09-22 21:04:34 +0530

    compile for other platforms too

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ if [ -z "$version" ]; then
     exit -1
 fi
 
-go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o kubectl-modify-secret main.go
+mkdir -p _dist
+env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
+env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-darwin-amd64-$version      main.go
+env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-linux-amd64-$version       main.go
