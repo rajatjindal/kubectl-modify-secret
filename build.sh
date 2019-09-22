7a65282b7a3a633eb17d0f4c608b15b81ebb921f
Rajat Jindal <rajatjindal83@gmail.com>
2019-09-22 21:04:49 +0530

    compile for other platforms too

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ if [ -z "$version" ]; then
 fi
 
 mkdir -p _dist
-env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
+# env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
 env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-darwin-amd64-$version      main.go
 env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-linux-amd64-$version       main.go
