9e712cc72c686a7c9659a41966856f27eced6249
Rajat Jindal <rajatjindal83@gmail.com>
2019-09-23 16:43:08 +0530

    review comments changes

--- build.sh
+++ build.sh
@@ -10,12 +10,13 @@ fi
 rm -rf _dist || true
 mkdir -p _dist
 # env GOOS=windows GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/kubectl-modify-secret-windows-amd64-$version.exe main.go
-env GOOS=darwin  GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/darwin-amd64/kubectl-modify-secret      main.go
+env GOOS=darwin  GOARCH=amd64 go build -ldflags "-w -s -X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/darwin-amd64/kubectl-modify-secret      main.go
 cd _dist/darwin-amd64/ && tar -cvzf darwin-amd64-$version.tar.gz kubectl-modify-secret
-
+shasum -a 256 darwin-amd64-$version.tar.gz | awk '{print $1}' > sha256-darwin-amd64-$version
 cd -
 
-env GOOS=linux   GOARCH=amd64 go build -ldflags "-X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/linux-amd64/kubectl-modify-secret       main.go
+env GOOS=linux   GOARCH=amd64 go build -ldflags "-w -s -X github.com/rajatjindal/kubectl-modify-secret/cmd.Version=$version" -o _dist/linux-amd64/kubectl-modify-secret       main.go
 cd _dist/linux-amd64/  && tar -cvzf linux-amd64-$version.tar.gz kubectl-modify-secret
+shasum -a 256 linux-amd64-$version.tar.gz | awk '{print $1}' > sha256-linux-amd64-$version
 
 cd -
--- pkg/editor/editor.go
+++ pkg/editor/editor.go
@@ -1,12 +1,19 @@
 package editor
 
 import (
+	"errors"
 	"os"
 	"os/exec"
 )
 
+//Edit opens the editor
 func Edit(file string) error {
-	cmd := exec.Command("vim", file)
+	editorFromEnv := os.Getenv("EDITOR")
+	if editorFromEnv == "" {
+		return errors.New("ENV variable $EDITOR not set")
+	}
+
+	cmd := exec.Command(editorFromEnv, file)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
